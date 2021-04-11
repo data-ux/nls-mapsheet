f8f54642dbf8b5d6a478917fd3953f5c7d714b50
Jan Wolski <jan.wolski@trimble.com>
2021-04-11 22:45:40 +0300

    Parametrize mount location

--- test/custom-entrypoint.sh
+++ test/custom-entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 run_test() {
     echo "Running nls_mapsheet test! Loading function to DB."
-    psql -U postgres -f /nls_mapsheet/nls_mapsheet.sql
+    psql -U postgres -f $1/nls_mapsheet.sql
     echo "Restoring test data to DB."
-	pg_restore -d postgres /nls_mapsheet/test/data.dump
+	pg_restore -d postgres $1/test/data.dump
     echo "Testing all mapsheets"
     psql -d postgres -c "do \$\$ begin assert (SELECT count(*) FROM mapsheet WHERE NOT st_equals(geom, nls_mapsheet(grid_ref))) = 0, 'Not all mapsheets pass!';end;\$\$;"
     echo "Test PASS"
@@ -24,7 +24,7 @@ fi
 docker_init_database_dir
 pg_setup_hba_conf
 
-docker_temp_server_start "$@" -c max_locks_per_transaction=256
+docker_temp_server_start
 docker_setup_db
 docker_process_init_files /docker-entrypoint-initdb.d/*
 run_test
